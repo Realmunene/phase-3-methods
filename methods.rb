844ae5a4808897ea6cb7783a6a70258adbaa494f
Realmunene <josep.mwangi@student.moringaschool.com>
2022-10-18 21:31:29 +0300

    modified

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    print "Hello, programmer!"
+end
+
+def greet name
+puts "Hello, #{name}!"
+end
+greet("Naureen!")
+greet("Jimmy!")
+def greet_with_default (name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add (num1, num2)
+    return num1 + num2
+end
+sum = add(1,2)
+
+def halve number
+    if (typeof number !== "number")
+        return null;
+    end
+return number/2
+end
+halve(4)
